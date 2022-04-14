bd4c5ec5060cc2ab1138fa3cc34941668eb6a166
ilker <ilkerdagdeviren@yahoo.com>
2022-04-15 01:38:07 +0300

    day39: Recap

---
+++ src/day39_Recap/cydeoTask/Person.java
@@ -0,0 +1,72 @@
+package day39_Recap.cydeoTask;
+
+public class Person {
+    private String name; // instance var ise encapsulate etm icin private kullaniyorum.
+    private int age; // static var ise public kullanirim.
+    private char gender;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public Person(String name, int age, char gender) {
+        setName(name);
+        setAge(age);
+        setGender(gender);
+    }
+    
+    public void eat(){
+        System.out.println(name+" is eating");
+    }
+
+    public void drink(){
+        System.out.println(name+" is drinking");
+    }
+
+    public void sleep(){
+        System.out.println(name+" is sleeping");
+    }
+
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+
+}
+
+
+/*
+1. Create a class named Person:
+            Variables:
+                name, age, gender
+            Encapsulate all the fields
+            Add a constructor to set all the fields
+            Methods:
+                eat()
+                drink()
+                sleep()
+                toString()
+ */
